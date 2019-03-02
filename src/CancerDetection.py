693f05880fcbb294dc66b493aef6ac61bbd5d8a1
Juan Isern Ghosn <juan.isern95@gmail.com>
2019-03-02 18:49:22 +0100

    correct train/test split

--- src/CancerDetection.py
+++ src/CancerDetection.py
@@ -1,6 +1,6 @@
 import scipy.io
 import numpy as np
-from sklearn.model_selection import KFold
+from sklearn import metrics
 import random
 import gpflow as gp
 
@@ -17,18 +17,37 @@ def gp_cross_val(data):
     positive_ex = data['Malign_folds'].flatten()
 
     for i in range(0,5):
-        train_index = range(0,5)
-        del train_index[i]
+        train_index = list(range(0,5))
+        train_index.remove(i)
 
         train_neg = np.vstack([negative_ex[i][0] for i in train_index])
         train_pos = np.vstack([positive_ex[i][0] for i in train_index])
+
         test_neg = negative_ex[i][0]
         test_pos = positive_ex[i][0]
+        X_test = np.concatenate((test_neg, test_pos))
+        Y_test = np.append([-1] * test_neg.shape[0], np.ones(test_pos.shape[0]))
 
-        for part in partition(range(0,train_neg.shape[0]), 4):
-            X = train_neg
+
+        for part in partition(list(range(0,train_neg.shape[0])), 4):
+            X = np.concatenate((train_neg[part],train_pos))
+            Y = np.append([-1] * len(part),np.ones(len(train_pos)))
+
+            idx = np.random.permutation(len(X))
+            X, Y = X[idx], Y[idx]
+
+            m = gp.models.SVGP(X=X, Y=Y, kern=gp.kernels.Linear, likelihood=gp.likelihoods.Gaussian)
+            m.feature.set_trainable(False)
+            gp.train.ScipyOptimizer().minimize(m, maxiter=20)
+            m.feature.set_trainable(True)
+            gp.train.ScipyOptimizer(options=dict(maxiter=200)).minimize(m)
+
+
+            p = m.predict_y(X_test)
+
             print("hola")
-            #gp.models.SVGP(kern=gp.kernels.Linear)
+
+
 
 
 def partition (list_in, n):
