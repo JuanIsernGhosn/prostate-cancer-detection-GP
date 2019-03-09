be8ed53801ca7d41d816abaf5f550e60ae6573a8
Juan Isern Ghosn <juan.isern95@gmail.com>
2019-03-09 17:33:41 +0100

    Confussion matrix implemented

--- src/CancerDetection.py
+++ src/CancerDetection.py
@@ -5,34 +5,87 @@ import random
 import gpflow as gp
 import _pickle as cPickle
 import matplotlib.pyplot as plt
+from itertools import product
 
 DATA_PATH = '../Datos.mat'
 
-def __main__():
 
+def __main__():
+    '''
     neg_fold, pos_fold = read_mat(DATA_PATH)
     neg_fold_norm, pos_fold_norm = norm_data(neg_fold, pos_fold)
 
-    auc = gp_cross_val(neg_fold_norm, pos_fold_norm)
+    roc_gaussian = gp_cross_val(neg_fold_norm, pos_fold_norm, krnl="Gaussian")
+    write_data(roc_gaussian, "auc_gaussian.pkl")
 
-    write_data(auc, "auc.pkl")
-    fpr, tpr, tresholds = read_data("auc.pkl")
+    roc_linear = gp_cross_val(neg_fold_norm, pos_fold_norm, krnl="Linear")
+    write_data(roc_linear, "auc_linear.pkl")
+    '''
 
-    plot_roc(fpr, tpr)
+    auc_linear = read_data("auc_gaussian.pkl")
+    plot_cm_grid(auc_linear.get("cm"), title='Núcleo Gaussiano')
 
+def plot_cm_grid(cms, title):
+    size = 321
+    plt.figure(figsize=(10, 10))
+    plt.suptitle('Matrices de confusión: '+title)
+    for i, cm in enumerate(cms):
+        plt.subplot(size + i)
+        plot_confusion_matrix(cm, target_names=['Sana', 'Cáncer'], title='Fold '+str(i+1))
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.93)
+    plt.show()
+
+def plot_confusion_matrix(cm, target_names=['0', '1'], title='Confusion matrix',normalize=False):
+    """Plot confusion matrix with better aesthetic.
+    Args:
+        (int[][]) cm: Confusion matrix to plot.
+        (String[]) target_names: Name of the matrix labels
+        (String) title: Title to print above the matrix.
+        (bool) normalize: Set if is matrix normalization required.
+    """
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    cmap = plt.get_cmap('Blues')
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('Etiqueta real')
+    plt.xlabel('Etiqueta predicha')
 
 def plot_roc(fpr_folds, tpr_folds):
     plt.figure()
-    for fpr, tpr in zip(fpr_folds, tpr_folds ):
+    for fpr, tpr in zip(fpr_folds, tpr_folds):
         plt.plot(fpr, tpr)
     plt.show()
 
 
 def norm_data(folds_neg, folds_pos):
-
     orig_index_n = get_fold_index(folds_neg)
     join_neg = np.vstack([fold for fold in folds_neg])
-    orig_index_p = get_fold_index(folds_pos, ini = (join_neg.shape[0]-1))
+    orig_index_p = get_fold_index(folds_pos, ini=(join_neg.shape[0]))
     join_pos = np.vstack([fold for fold in folds_pos])
 
     join = np.concatenate((join_neg, join_pos))
@@ -40,21 +93,26 @@ def norm_data(folds_neg, folds_pos):
 
     return [join_n[fold_idx] for fold_idx in orig_index_n], [join_n[fold_idx] for fold_idx in orig_index_p]
 
+
 def get_fold_index(folds, ini=0):
     orig_index = []
     for n, fold in enumerate(folds):
-        ini = ini+folds[n-1].shape[0] if n > 0 else ini
-        orig_index.append(list(range(ini,ini+fold.shape[0])))
+        ini = ini + folds[n - 1].shape[0] if n > 0 else ini
+        orig_index.append(list(range(ini, ini + fold.shape[0])))
     return orig_index
 
-def gp_cross_val(neg_fold, pos_fold):
 
-    tpr = []
-    fpr = []
-    roc_auc = []
+def gp_cross_val(neg_fold, pos_fold, krnl = "Linear", treshold = 0.5):
+    recall_1 = []
+    recall_2 = []
+    one_m_spec = []
+    spec_rec_auc = []
+    prec = []
+    prec_rec_auc = []
+    cms = []
 
-    for i in range(0,5):
-        train_index = list(range(0,5))
+    for i in range(0, 5):
+        train_index = list(range(0, 5))
         train_index.remove(i)
 
         train_neg = np.vstack([neg_fold[j] for j in train_index])
@@ -64,19 +122,22 @@ def gp_cross_val(neg_fold, pos_fold):
         test_pos = pos_fold[i]
 
         x_test = np.concatenate((test_neg, test_pos))
-        y_test = np.append(np.zeros(test_neg.shape[0]), np.ones(test_pos.shape[0]))
+        y_test = np.append([-1]*test_neg.shape[0], np.ones(test_pos.shape[0]))
 
         probs_fold = []
 
-        for part in partition(list(range(0,train_neg.shape[0])), 4):
-            x = np.concatenate((train_neg[part],train_pos))
-            y = np.append(np.zeros(len(part)), np.ones(len(train_pos)))
+        for part in partition(list(range(0, train_neg.shape[0])), 4):
+            x = np.concatenate((train_neg[part], train_pos))
+            y = np.append([-1]*len(part), np.ones(len(train_pos)))
             y = y.reshape(-1, 1)
 
             idx = np.random.permutation(len(x))
             x, y = x[idx], y[idx]
 
-            m = gp.models.VGP(X=x, Y=y, kern=gp.kernels.RBF(x.shape[1]), likelihood=gp.likelihoods.Bernoulli())
+            if krnl == "Linear":
+                m = gp.models.VGP(X=x, Y=y, kern=gp.kernels.Linear(x.shape[1]), likelihood=gp.likelihoods.Bernoulli())
+            else:
+                m = gp.models.VGP(X=x, Y=y, kern=gp.kernels.RBF(x.shape[1]), likelihood=gp.likelihoods.Bernoulli())
 
             gp.train.ScipyOptimizer().minimize(m, maxiter=300)
 
@@ -86,22 +147,30 @@ def gp_cross_val(neg_fold, pos_fold):
 
         mean_probs = np.mean(probs_fold, axis=0).flatten()
 
-        fpr_v, tpr_v, thresholds = metrics.roc_curve(y_test, mean_probs)
-        tpr.append(tpr_v)
-        fpr.append(fpr_v)
-        roc_auc.append(metrics.auc(fpr_v, tpr_v))
+        one_m_spec_v, recall_v, thresholds = metrics.roc_curve(y_test, mean_probs)
+        one_m_spec.append(one_m_spec_v)
+        recall_1.append(recall_v)
+        spec_rec_auc.append(metrics.auc(one_m_spec_v, recall_v))
 
-    return (fpr, tpr, roc_auc)
+        prec_v, recall_v, thresholds = metrics.precision_recall_curve(y_test, mean_probs)
+        prec.append(prec_v)
+        recall_2.append(recall_v)
+        prec_rec_auc.append(metrics.auc(recall_v, prec_v))
+
+        y_pred = [1 if prob > treshold else -1 for prob in mean_probs]
+
+        cms.append(metrics.confusion_matrix(y_test, y_pred))
+
+    return {"one_minus_specificity": one_m_spec, "recall_1": recall_1, "one_m_spe_recall_auc": spec_rec_auc,
+            "precision": prec, "recall_2": recall_2, "prec_rec_auc": prec_rec_auc, "cm": cms}
 
 
 def read_data(path):
     """Read .pkl file saved locally.
-
     Args:
         (String) path: Path of the file to load.
     Returns:
         (Object): Read file object.
-
     """
     with open(path, 'rb') as fid:
         return cPickle.load(fid)
@@ -109,21 +178,22 @@ def read_data(path):
 
 def write_data(data, path):
     """Save .pkl file locally.
-
     Args:
         (Object): Object to be saved.
     """
     with open(path, 'wb') as fid:
         cPickle.dump(data, fid)
 
-def partition (list_in, n):
+
+def partition(list_in, n):
     random.shuffle(list_in)
     return [list_in[i::n] for i in range(n)]
+
 
 def read_mat(path):
     mat = io.loadmat(path)
     negative_ex = mat['Healthy_folds'].flatten()
     positive_ex = mat['Malign_folds'].flatten()
-    negative_ex = [negative_ex[i][0] for i in list(range(0,5))]
-    positive_ex = [positive_ex[i][0] for i in list(range(0,5))]
+    negative_ex = [negative_ex[i][0] for i in list(range(0, 5))]
+    positive_ex = [positive_ex[i][0] for i in list(range(0, 5))]
     return negative_ex, positive_ex
